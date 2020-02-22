b507e5ac1622259e599a43ce2af88e515c0ee8a4
Alexander Bidenko <alexbidenko1998@gmail.com>
2020-02-22 22:24:39 +0300

    first commit

---
+++ app/Http/Controllers/PassportController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class PassportController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('PlayAndLearn')->accessToken;
+
+        return response()->json(['token' => $token], 200);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        if (auth()->attempt($credentials)) {
+            $token = auth()->user()->createToken('TutsForWeb')->accessToken;
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['error' => 'UnAuthorised'], 401);
+        }
+    }
+
+    public function details()
+    {
+        return response()->json(['user' => auth()->user()], 200);
+    }
+}
