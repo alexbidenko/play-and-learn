79fecfe5a5de9949217354faacc87874d17f5741
Alexander Bidenko <alexbidenko1998@gmail.com>
2020-02-23 21:59:20 +0300

    Bugfix

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -11,10 +11,9 @@ class Authenticate extends Middleware
      * Get the path the user should be redirected to when they are not authenticated.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return string|null
      */
     protected function redirectTo($request)
     {
-        return response()->json('Unauthorized request', Response::HTTP_UNAUTHORIZED);
+        abort(Response::HTTP_UNAUTHORIZED);
     }
 }
