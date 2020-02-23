4c73a4c0aea69ec938eb4b20b53eef0eef58b9ee
Alexander Bidenko <alexbidenko1998@gmail.com>
2020-02-23 22:07:38 +0300

    Bugfix

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Methods: POST, GET, PUT, DELETE, PATCH, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, X-Token-Auth, Authorization');
 
 /**
  * Laravel - A PHP Framework For Web Artisans
