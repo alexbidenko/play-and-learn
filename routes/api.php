b2f6ff8cf622404322a68b06ba8f0a331f43bddf
Alexander Bidenko <alexbidenko1998@gmail.com>
2020-02-23 22:13:14 +0300

    Bugfix

--- routes/api.php
+++ routes/api.php
@@ -13,34 +13,31 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('cors')->group(function () {
+Route::post('sign-in', 'PassportController@login');
+Route::post('sign-up', 'PassportController@register');
 
-    Route::post('sign-in', 'PassportController@login');
-    Route::post('sign-up', 'PassportController@register');
+Route::middleware('auth:api')->group(function () {
 
-    Route::middleware('auth:api')->group(function () {
+    Route::get('user', 'PassportController@details');
 
-        Route::get('user', 'PassportController@details');
+    Route::get('examinations', 'RedactionController@examinations');
+    Route::get('subjects', 'RedactionController@subjects');
+    Route::get('levels', 'RedactionController@levels');
+    Route::get('tasks', 'RedactionController@tasks');
 
-        Route::get('examinations', 'RedactionController@examinations');
-        Route::get('subjects', 'RedactionController@subjects');
-        Route::get('levels', 'RedactionController@levels');
-        Route::get('tasks', 'RedactionController@tasks');
+    Route::post('examinations', 'RedactionController@addExamination');
+    Route::post('subjects', 'RedactionController@addSubject');
+    Route::post('levels', 'RedactionController@addLevel');
+    Route::post('tasks', 'RedactionController@addTask');
 
-        Route::post('examinations', 'RedactionController@addExamination');
-        Route::post('subjects', 'RedactionController@addSubject');
-        Route::post('levels', 'RedactionController@addLevel');
-        Route::post('tasks', 'RedactionController@addTask');
+    Route::put('examinations/{id}', 'RedactionController@updateExamination');
+    Route::put('subjects/{id}', 'RedactionController@updateSubject');
+    Route::put('levels/{id}', 'RedactionController@updateLevel');
+    Route::put('tasks/{id}', 'RedactionController@updateTask');
 
-        Route::put('examinations/{id}', 'RedactionController@updateExamination');
-        Route::put('subjects/{id}', 'RedactionController@updateSubject');
-        Route::put('levels/{id}', 'RedactionController@updateLevel');
-        Route::put('tasks/{id}', 'RedactionController@updateTask');
-
-        Route::delete('examinations/{id}', 'RedactionController@deleteExamination');
-        Route::delete('subjects/{id}', 'RedactionController@deleteSubject');
-        Route::delete('levels/{id}', 'RedactionController@deleteLevel');
-        Route::delete('tasks/{id}', 'RedactionController@deleteTask');
-    });
-
+    Route::delete('examinations/{id}', 'RedactionController@deleteExamination');
+    Route::delete('subjects/{id}', 'RedactionController@deleteSubject');
+    Route::delete('levels/{id}', 'RedactionController@deleteLevel');
+    Route::delete('tasks/{id}', 'RedactionController@deleteTask');
 });
+
