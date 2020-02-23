8c61bf69998e86d3da273a27e2aed8848668d986
Alexander Bidenko <alexbidenko1998@gmail.com>
2020-02-24 00:37:16 +0300

    Bugfix

---
+++ public/precache-manifest.836cc2aec3654caa3ec3ff9afa1f4014.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8b758ab9b060288a2db",
+    "url": "/css/chunk-vendors.857f21a4.css"
+  },
+  {
+    "revision": "c2cf1e3fabcb11e5249e745d59c1db2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aac75cdeb05de5cb0507",
+    "url": "/js/app.1f068676.js"
+  },
+  {
+    "revision": "f8b758ab9b060288a2db",
+    "url": "/js/chunk-vendors.7025d42c.js"
+  },
+  {
+    "revision": "a307285a4f878a11ac2af1a1d21897f7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
